4544cdd637068e4f24dae7a35dbfb6b8516e6d24
MariemOsama <mariem.osama152@gmail.com>
2017-04-05 04:06:59 +0200

    Create order and list latest orders

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,6 +1,24 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var Order = require('../models/order');
+
 module.exports = {
 	create: function(userEmail, type, restaurant, friends, menuImage) {
-		throw 'Not yet implemented';
+
+		var order = new Order({
+				owner: userEmail,
+				type: type,
+				restaurant: restaurant,
+				requests: friends,
+				menuImage: menuImage,
+				orders: [],
+				status: 'waiting'
+		});
+    order.save(function(err,data){
+      if(!err){
+				console.log(data);
+      }
+    });
 	},
 	accept: function(userEmail, orderID) {
 		throw 'Not yet implemented';
@@ -14,9 +32,19 @@ module.exports = {
 	cancel: function(userEmail, orderID) {
 		throw 'Not yet implemented';
 	},
+
+
 	latestOrders: function(userEmail) {
-		throw 'Not yet implemented';
+		Order.find({'owner': userEmail},function(err,data){
+			if(!err){
+				console.log(data);
+			}
+		}).
+		limit(5).
+		sort({createdAt: -1});
 	},
+
+
 	friendsActivity: function(friendsEmails) {
 		throw 'Not yet implemented';
 		// Map Reduce

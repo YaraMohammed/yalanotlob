0a5265bf13e86cfd985f5b120c02023c8840c863
MariemOsama <mariem.osama152@gmail.com>
2017-04-08 11:35:08 +0200

    Send Request to speciefic friends(Not Complete)

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -4,13 +4,16 @@ var User = require('../models/user');
 
 module.exports = {
 	// create new order functions
-	create: function(userEmail, type, restaurant, friends, menuImageUrl) {
+	create: function(userEmail, type, restaurant, friends, menuImageUrl)
+	{
 		var reqs = {};
 		var i = 0;
-		for (var friend of friends) {
+		for (var friend of friends)
+		{
 
 			friend = new Buffer(friend).toString('base64');
-			User.findOne({'_id': new Buffer(friend, 'base64').toString('ascii')},function (err , data) {
+			User.findOne({'_id': new Buffer(friend, 'base64').toString('ascii')},function (err , data)
+			{
 				if(data != null)
 				{
 					var d = data._id;
@@ -27,22 +30,40 @@ module.exports = {
 
 				}
 				i++;
-				if(i == friends.length){
+				if(i == friends.length)
+				{
 					console.log('Reqs', reqs);
-					var order = new Order({
-						owner: userEmail,
-						type: type,
-						restaurant: restaurant,
-						requests: reqs,
-						menuImageUrl: menuImageUrl,
-						orders: [],
-						status: 'waiting'
-					});
-					order.save(function(err,data){
-						if(!err){
-							console.log(data);
+					var order = new Order(
+						{
+							owner: userEmail,
+							type: type,
+							restaurant: restaurant,
+							requests: reqs,
+							menuImageUrl: menuImageUrl,
+							orders: [],
+							status: 'waiting'
 						}
-					});
+					);
+					order.save(function(err,data)
+					{
+						if(!err)
+						{
+							console.log(data);
+							for(var key in reqs)
+							{
+								if(reqs.hasOwnProperty(key))
+								{
+									var id = data._id;
+									var uid = new Buffer(key, 'base64').toString('ascii');
+									User.update({'_id':new Buffer(key, 'base64').toString('ascii')},{$push:{'orderRequests':id}});
+									console.log('Key ',key,' Reqs[Key]',reqs[key],'Order ID ',id,'userId ',uid);
+
+								}
+							}
+						}
+
+					}
+				);
 				}
 			});
 		}
@@ -50,28 +71,36 @@ module.exports = {
 	},
 
 	// get order details
-	get: function(userEmail, orderID, cb) {
-		Order.findOne({
-			'_id': orderID
-		}, (err, order) => {
+	get: function(userEmail, orderID, cb)
+	{
+		Order.findOne(
+			{
+				'_id': orderID
+			}, (err, order) => {
 			cb(order);
-		});
+		}
+	);
 	},
 
 	// get order details
-	get: function(userEmail, orderID, cb) {
-		Order.findOne({
-			'_id': orderID
-		}, (err, order) => {
+	getOrder: function(userEmail, orderID, cb)
+	{
+		Order.findOne(
+			{
+				'_id': orderID
+			}, (err, order) => {
 			cb(order);
-		});
+		}
+	);
 	},
 
 	// accept order invitation
-	accept: function(userEmail, orderID) {
-		Order.findOne({
-			'_id': orderID
-		}, (err, order) => {
+	accept: function(userEmail, orderID)
+	{
+		Order.findOne(
+			{
+				'_id': orderID
+			}, (err, order) => {
 			userEmail = new Buffer(userEmail).toString('base64');
 			if (order.requests[userEmail] == undefined)
 			{
@@ -81,51 +110,69 @@ module.exports = {
 			{
 				order.requests[userEmail] = 'accepted';
 			}
-		});
+		}
+	);
 	},
 
 	// add an item to order
-	addItem: function(userEmail, orderID, item, amount, price, comment, cb) {
+	addItem: function(userEmail, orderID, item, amount, price, comment, cb)
+	{
 
-		var itoma ={
+		var itoma = {
 			owner: userEmail,
 			item: item,
 			amount: amount,
 			price: price,
 			comment: comment
 		};
-		Order.findOneAndUpdate({'_id': orderID},{$push:{'orders':itoma}},function (err,data) {
-			if(!err){
+		Order.findOneAndUpdate({'_id': orderID},{$push:{'orders':itoma}},function (err,data)
+		{
+			if(!err)
+			{
 				console.log(data);
-			}else{
+			}
+			else
+			{
 				console.error(err);
 			}
 			cb(err);
-		});
+		}
+	);
 	},
 
 	// change order status to finished
-	finish: function(userEmail, orderID) {
-		Order.findOneAndUpdate({'_id': orderID, 'owner': userEmail},{$set:{'status': 'finished'}},function (err,data) {
-			if(!err){
+	finish: function(userEmail, orderID)
+	{
+		Order.findOneAndUpdate({'_id': orderID, 'owner': userEmail},{$set:{'status': 'finished'}},function (err,data)
+		{
+			if(!err)
+			{
 				console.log(data);
 			}
-		});
+		}
+	);
 	},
 
 	// change order status to cancelled
-	cancel: function(userEmail, orderID) {
-		Order.remove({'_id':orderID, 'owner': userEmail},function (err,data) {
-			if(!err){
+	cancel: function(userEmail, orderID)
+	{
+		Order.remove({'_id':orderID, 'owner': userEmail},function (err,data)
+		{
+			if(!err)
+			{
 				console.log(data);
 			}
-		});
+		}
+	);
 	},
 
 	// lists 5 recent orders
-	latestOrders: function(userEmail, cb) {
-		Order.find({'owner': userEmail},function(err,data){
-			if(!err){
+	latestOrders: function(userEmail, cb)
+	{
+		Order.find({'owner': userEmail},function(err,data)
+		{
+			if(!err)
+			{
 				cb(data);
 			}
 		}).

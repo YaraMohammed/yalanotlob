15e56f53fb211ddd2e1a4c3cfdad5a00b74f9f18
Yara Mohammed <yara.mohammed174@gmail.com>
2017-04-17 23:59:46 +0200

    fix small bug

--- src/static/assets/js/socketHandler.js
+++ src/static/assets/js/socketHandler.js
@@ -7,7 +7,7 @@ if(getCookie('token')){
 //listen to notification
 	socket.on('notification', function(data){
 		if(data.type == 'orderJoinRequest' && data.senderName != undefined){
-			$('#notifications').prepend('<li id="'+data.orderID+'"><a href=/user/'+data.sender+'>'+data.senderName+' </a> has invited you to his order. <a href=/order/'+data.orderID+'/accept>Join</a></li>');
+			$('#notifications').prepend('<li><a href=/user/'+data.sender+'>'+data.senderName+' </a> has invited you to his order. <a href=/order/'+data.orderID+'/accept>Join</a></li>');
 		} else if (data.type == 'orderAccept') {
 			$('#notifications').prepend('<li><a href=/user/'+data.sender+'>'+data.senderName+' </a> has accepted invitation to <a href=/order/'+data.orderID+'>your order</a>.</li>');
 		} else if (data.type == 'notifyFinished') {

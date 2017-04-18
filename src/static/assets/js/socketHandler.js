921fadaf09c467f10e4098a495efbdc3f51a3213
Ahmed Mokhtar <mokhtar92@gmail.com>
2017-04-18 09:58:53 +0200

    small gui fixes

--- src/static/assets/js/socketHandler.js
+++ src/static/assets/js/socketHandler.js
@@ -8,20 +8,20 @@ if(getCookie('token')){
 	socket.on('notification', function(data){
 		if(data.type == 'orderJoinRequest' && data.senderName != undefined){
 			$('#notifications').prepend('<li><a href=/user/'+data.sender+'>'+data.senderName+' </a> has invited you to his order. <a href=/order/'+data.orderID+'/accept>Join</a></li>');
-			$('#badge').html(parseInt($('#badge').html())+1)
+			$('#badge').html(Number($('#badge').html())+1)
 		} else if (data.type == 'orderAccept') {
 			$('#notifications').prepend('<li><a href=/user/'+data.sender+'>'+data.senderName+' </a> has accepted invitation to <a href=/order/'+data.orderID+'>your order</a>.</li>');
-			$('#badge').html(parseInt($('#badge').html())+1)
+			$('#badge').html(Number($('#badge').html())+1)
 		} else if (data.type == 'notifyFinished') {
 			location.reload();
 		} else if (data.type == 'notifyCancelled'){
 			location.reload();
-		} else if (data.type == 'notifyFriend'){			        
+		} else if (data.type == 'notifyFriend'){
 			$('#activity').prepend("<h4><dl><dt><a href=/user/"+data.sender+'><span class="label label-primary">'+data.senderName+'</span></a></dt><dd>- has created an order for '+data.orderType+ ' from ' + data.restaurant+'</dd></dl></h4>')
 		}
 	});
 }
 
 $('#notificate').on('click',function(){
-	$('#badge').html(0);
+	$('#badge').html('');
 })

4482766ace782f0c39c9c51b9aa23c7c83d25bb7
Ahmed Mokhtar <mokhtar92@gmail.com>
2017-04-18 06:27:18 +0200

    doing things

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,12 +1,33 @@
 /* LIBs */
 var express = require('express');
+var cookieParser = require('cookie-parser');
+var user = require('../controllers/user');
 
 /* VARs */
 var router = express.Router();
 
 /* ROUTING */
-router.use((req, res, next) => {
-	throw 'Not yet implemented';
+router.get('/get-users/:entity_id', cookieParser(), (req, res) => {
+	user.get(req.params['entity_id'], (u) => {
+		if (u) {
+			res.json([u]);
+		} else {
+			var email = user.userEmail(req.cookies.token);
+			if (email) {
+				user.get(email, (u) => {
+					if (u && u.groups.hasOwnProperty(req.params['entity_id'])) {
+						user.listFriends(u.groups[req.params['entity_id']], (err, u) => {
+							res.json(u);
+						});
+					} else {
+						res.json([]);
+					}
+				});
+			} else {
+				res.json([]);
+			}
+		}
+	});
 });
 
 /* EXPORTING */
